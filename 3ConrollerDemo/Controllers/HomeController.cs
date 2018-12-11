bf865cc03389fa64ece22486694cfabf9ce26541
jesper <admin@TA-826-05>
2018-12-11 11:34:56 +0100

    Controllers

---
+++ 3ConrollerDemo/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _3ConrollerDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test1()
+        {
+            return Content("12");
+        }
+
+        public ActionResult Test2()
+        {
+            return Content("1,2,3", "application/CSV");
+        }
+
+        public ActionResult Test3()
+        {
+            return Redirect("http://www.google.dk");
+        }
+
+        public ActionResult Test4()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Test5()
+        {
+
+
+            return Json(Person.Personer(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Test6()
+        {
+
+
+            return View();
+        }
+
+
+
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+
+        public static List<Person> Personer()
+        {
+            List<Person> p = new List<Person>()
+            {
+                new Person(){ Id=1, Alder = 10, Navn = "asdf"},
+                new Person(){ Id=2, Alder = 30, Navn = "sdfdfsghegsgd"},
+                new Person(){ Id=3, Alder = 40, Navn = "dæflkgj lsdfkj lsdfkj dlsj"}
+            };
+            return p;
+        }
+    }
+
+}
+
---
+++ 3ConrollerDemo/Script/Test6.js
@@ -0,0 +1,22 @@
+﻿var knap = jQuery("#minknap");
+console.log(knap);
+
+knap.click(function () {
+    //console.log("12");
+   // $.getJSON("")
+
+    //$.getJSON("/Home/Test5").done(function (data) {
+    //    console.log(data);
+    //});
+    $.getJSON("/Home/Test5").done(function (data) {
+        var ol = $("#minliste");
+        ol.empty();
+
+        for (var i = 0; i < data.length; i++) {
+            var li = $("<li />").html(data[i].Navn);
+            ol.append(li);
+        }
+    });
+});
+
+
