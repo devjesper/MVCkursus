9a7afdd84e24e209d3e3c8b96f307973ea7d0be5
jesper <admin@TA-826-05>
2018-12-11 15:43:06 +0100

    dag2-mere

---
+++ 4FilterDemon/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using _4FilterDemon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4FilterDemon.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [OutputCache(Duration =10)]
+        public ActionResult Index()
+        {
+            ViewBag.Klokke = DateTime.Now.ToLongTimeString();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RetPerson()
+        {
+            string sti = Server.MapPath("~/app_data/person.json");
+            Person p;
+            if (System.IO.File.Exists(sti))
+            {
+                string indhold = System.IO.File.ReadAllText(sti);
+                p = Newtonsoft.Json.JsonConvert.DeserializeObject<Person>(indhold);
+            }
+            else
+            {
+                p = new Person() {Id=1};
+            }
+            
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult RetPerson(Person p)
+        {
+            if (ModelState.IsValid)
+            {
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(p);
+                string sti = Server.MapPath("~/app_data/person.json");
+
+                System.IO.File.WriteAllText(sti, json);
+            }
+            return View();
+        }
+
+        
+    }
+}
---
+++ 4FilterDemon/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _4FilterDemon.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+        public bool ErILive { get; set; }
+    }
+}
