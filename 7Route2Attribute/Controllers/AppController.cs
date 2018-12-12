b68317c35616229fc83969f5faee4c9c0d8a41a4
jesper <admin@TA-826-05>
2018-12-12 12:33:58 +0100

    dag3-route2

---
+++ 7Route2Attribute/Controllers/AppController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _7Route2Attribute.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: App
+        [HttpGet]
+        [Route("~/")]
+        [Route("~/Forside")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        
+        [Route("~/Salg/{kundeid:int}")]
+        public ActionResult Salg(int kundeid)
+
+        {
+            ViewBag.kundeid = kundeid;
+            return View();
+        }
+    }
+}
